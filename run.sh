cb36370b3720cc5f252e833268501bbe2d47fb7e
kshprenger <kostya@shprenger.com>
2026-01-05 15:36:22 +0100

    -

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-container run --name compiler-container --arch amd64 -it -v "$(pwd)":/root/compiler -p 37080:37080 compiler-image
+container run --name compiler-container --arch amd64 --rm -it -v "$(pwd)":/root/compiler -p 37080:37080 compiler-image
